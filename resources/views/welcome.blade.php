35bb89436e6c6c40e7149ac2a97d6c6374e89c89
Stefan <stefan.tatic@yahoo.com>
2018-01-31 23:52:48 +0100

    Auth controller created, routes set
    - registration page restricted to registered users

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel heheeh
                 </div>
 
                 <div class="links">
